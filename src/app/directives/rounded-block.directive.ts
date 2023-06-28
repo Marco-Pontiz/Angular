dc9aaec43324f4d0d8800cb1173e01a115379cca
Marco Pontiz <marcopontiz@gmail.com>
2023-06-28 16:51:51 -0300

    Aprendiendo usar ngStyle y implementando directives al proyecto

---
+++ src/app/directives/rounded-block.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appRoundedBlock]'
+})
+export class RoundedBlockDirective {
+  constructor (
+    private elementRef: ElementRef,
+    private renderer : Renderer2,
+    ) {
+    console.log(this.elementRef);
+
+    //this.renderer.setStyle(this.elementRef, "border-radius", "100px");
+  }
+
+}
