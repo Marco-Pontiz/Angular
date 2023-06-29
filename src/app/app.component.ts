cf9deac324fadb419cd992d64700a7e669ffe00e
Marco Pontiz <marcopontiz@gmail.com>
2023-06-29 10:39:43 -0300

    Agregando los alumnos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Proyecto-Angular';
+  sub_title = "Alumnos";
+  alumnos : {id: number, nombre: string, apellido: string}[] = [
+    {id: 1, nombre: "Nicolai" , apellido: "Belinsky"},
+    {id: 2, nombre: "Edward", apellido: "Richtofen"},
+    {id: 3, nombre: "Takeo", apellido: "Masaki"},
+    {id: 4, nombre: "Tank", apellido: "Dempsey"},
+    {id: 5, nombre: "Samantha", apellido: "Maxis"},
+    {id: 6, nombre: "Luz", apellido: "Noceda"}
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { MainWrapperComponent } from './main-wrapper/main-wrapper.component';
+import { RoundedBlockDirective } from './directives/rounded-block.directive';
+import { DraggableDirective } from './directives/draggable.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SidebarComponent,
-    MainWrapperComponent
+    MainWrapperComponent,
+    RoundedBlockDirective,
+    DraggableDirective
   ], //AppComponent pertenece a AppModule
   imports: [
     BrowserModule
